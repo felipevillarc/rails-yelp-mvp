072025a1f5f12309d141850642e90d40ac5bfa18
luis cavalcanti <felipevillarc@hotmail.com>
2020-09-17 16:43:51 -0300

    atividade 2 feita

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require_relative "../app/models/restaurant"
+
+Restaurant.create!(name:'japa', address: 'rua w, nº 1', category: 'japanese')
+Restaurant.create!(name:'china', address: 'rua w, nº 2', category: 'chinese')
+Restaurant.create!(name:'italy', address: 'rua w, nº 3', category: 'italian')
+Restaurant.create!(name:'frances', address: 'rua w, nº 4', category: 'french')
+Restaurant.create!(name:'belga', address: 'rua w, nº 5', category: 'belgian')
